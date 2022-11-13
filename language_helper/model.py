cb1a6e8fe2ec5f67044326bf3a1274a2662b8b57
mytnguyen26 <49925370+mytnguyen26@users.noreply.github.com>
2022-11-12 22:02:15 -0500

    feat: start structure of code

---
+++ language_helper/model.py
@@ -0,0 +1,19 @@
+
+"""
+Absorbing Markov Model takes current state as input,
+and output the next probable state
+
+For example:
+    - input: hi, how, are, you, doing
+    
+"""
+
+
+class Markov:
+    def __init__(state):
+        pass
+
+
+class RNN:
+    def __init__():
+        pass
