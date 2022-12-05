8ef3ea40f58dee04bcaea7216100111b5168f4a6
mytnguyen26 <49925370+mytnguyen26@users.noreply.github.com>
2022-12-04 19:38:25 -0500

    feat: add a data loader

--- language_helper/data.py
+++ language_helper/data.py
@@ -1,3 +1,25 @@
+"""
+This class load data from the local directory and return dataframe presentation
+of that data
+"""
+
+import os
+from typing import List
+import pandas as pd
 class Processing:
-    def __init__():
-        pass
+    def __init__(self, data_domain: str, data_dir: str = "../dataset/", delimiter: str = ","):
+        path = os.path.join(data_dir,data_domain)
+        self.delimiter = delimiter
+        self.file_path: List[str] = []
+        for file in os.listdir(path):
+            if file.endswith(("txt", "csv")):
+                self.file_path.append(os.path.join(path,file))
+    
+
+    def load(self):
+        dataframe = pd.DataFrame()
+        for file in self.file_path:
+            with open(file, "r") as file:
+                dataframe_stg = pd.read_csv(file, delimiter=self.delimiter)
+                dataframe = pd.concat([dataframe, dataframe_stg])
+        
