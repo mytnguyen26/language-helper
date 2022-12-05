7914e93b841c605694ea8a6f3e907dd847024bf9
mytnguyen26 <49925370+mytnguyen26@users.noreply.github.com>
2022-12-05 14:58:42 -0500

    feat: adding shell for evaluation methods

---
+++ language_helper/evaluation.py
@@ -0,0 +1,17 @@
+
+class Evaluation:
+    def __init__():
+        pass
+
+    def perplexity():
+        pass
+    
+    def blue():
+        """
+        BLUE is context-free evaluation metric for word based N-Grams.
+        This method evaluate the quality of the prediction.
+        """
+        pass
+
+    def run():
+        pass
